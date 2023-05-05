ad836fd3b3ebdc498e33500549c3be7fb6e7619e
haihung01 <104615629+haihung01@users.noreply.github.com>
2023-05-05 16:55:57 +0700

    update 222

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,16 +1,67 @@
-import React from 'react';
-import MovieList from '../components/movie/MovieList';
+import React, { useEffect, useState } from 'react';
+// import MovieList from '../components/movie/MovieList';
 import { fetcher } from '../config';
 import MovieCard from '../components/movie/MovieCard';
 import useSWR from 'swr';
+import useDebounce from '../hooks/useDebounce';
+import ReactPaginate from 'react-paginate';
+import { data } from 'autoprefixer';
 
 
 // https://api.themoviedb.org/3/movie/latest?api_key=<<api_key>>&language=en-US
+
+// search:  https://api.themoviedb.org/3/search/movie?api_key=
+
+
+
+const itemsPerPage = 20;
+
 const MoviePage = () => {
-    const { data } = useSWR(`https://api.themoviedb.org/3/movie/popular?api_key=e6ae53f90f39ad4d8584385a580100da`,
-        fetcher);
+
+
+    const [nextPage, setNextPage] = useState(1);
+
+    const [filter, setFilter] = useState("");
+
+    const [url, setUrl] = useState(`https://api.themoviedb.org/3/movie/popular?api_key=e6ae53f90f39ad4d8584385a580100da&page=${nextPage}`);
+
+    const filterDebounce = useDebounce(filter, 1000);
+
+    const handleFilterChange = (e) => {
+        setFilter(e.target.value);
+    }
+
+    const { data, error } = useSWR(url, fetcher);
+
+    const loading = !data && !error;
+
+    useEffect(() => {
+        if (filterDebounce) {
+            setUrl(`https://api.themoviedb.org/3/search/movie?api_key=e6ae53f90f39ad4d8584385a580100da&query=${filterDebounce}&page=${nextPage}`);
+        } else {
+            setUrl(`https://api.themoviedb.org/3/movie/popular?api_key=e6ae53f90f39ad4d8584385a580100da&page=${nextPage}`);
+        }
+    }, [filterDebounce, nextPage]);
+
 
     const movies = data?.results || [];
+
+    // react-paginate
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+
+    useEffect(() => {
+        if (!data || !data.total_pages) return;
+        const endOffset = itemOffset + itemsPerPage;
+        setPageCount(Math.ceil(data.total_pages / itemsPerPage));
+    }, [data, itemOffset])
+
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % data.total_pages;
+        setItemOffset(newOffset);
+        setNextPage(event.selected + 1);
+    };
 
 
 
@@ -23,6 +74,7 @@ const MoviePage = () => {
                         type='text'
                         className='w-full p-4 bg-slate-800 text-white outline-none'
                         placeholder='Type here to search...'
+                        onChange={handleFilterChange}
                     />
                 </div>
 
@@ -34,11 +86,29 @@ const MoviePage = () => {
                 </button>
             </div>
 
+            {loading && (
+                <div className='w-10 h-10 rounded-full border-4 border-primary border-t-transparent border-t-4 mx-auto animate-spin'>
+                </div>
+            )}
             <div className="grid grid-cols-4 gap-10">
-                {movies.length > 0 && movies.map(item => (
-                    <MovieCard key={item.id} item={item}></MovieCard>
-                ))}
+                {!loading &&
+                    movies.length > 0 && movies.map(item => (
+                        <MovieCard key={item.id} item={item}></MovieCard>
+                    ))}
             </div>
+            <div className="mt-10">
+                <ReactPaginate
+                    breakLabel="..."
+                    nextLabel="next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCount}
+                    previousLabel="< previous"
+                    renderOnZeroPageCount={null}
+                    className='paginate'
+                />
+            </div>
+
         </div>
     );
 };
